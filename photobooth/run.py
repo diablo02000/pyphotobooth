5fa19a93826ee72c0dcfb5cfbaa5d09d254fda94
Christophe <christophe.biguereau@gmail.com>
2017-11-26 17:57:11 +0100

    Add libraries version and test output directory before run apps.

--- photobooth/run.py
+++ photobooth/run.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
+import logging
+import os
+
+
+def set_logging(lvl=logging.INFO):
+    """
+    Set logging output.
+    :param lvl: Log level.
+    :type lvl: Int
+    """
+    logging.basicConfig(level=lvl)
+
 
 if __name__ == "__main__":
     # Create script arguments parser
@@ -9,3 +21,16 @@ if __name__ == "__main__":
 
     params = args_parse.parse_args()
 
+    # Check output directory
+    try:
+        if os.path.isdir(params.output):
+            with open("{}/{}".format(params.output, "photobooth.txt", 'w')) as f:
+                f.write("")
+
+            # delete test file
+            os.remove("{}/{}".format(params.output, "photobooth.txt"))
+
+        else:
+            os.makedirs(params.output)
+    except os.error as e:
+        logging.error("Failed to write in {}: {}".format(params.output, e))
