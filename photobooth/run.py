aea2756eca8b95f53974117d3c9fa022353773a7
Christophe <christophe.biguereau@gmail.com>
2017-12-06 17:18:03 +0100

    Fix error when try to write testing file.

--- photobooth/run.py
+++ photobooth/run.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
-from guizero import App, PushButton, Text, Picture
-from picamera import PiCamera
 import logging
 import os
 
@@ -19,31 +17,20 @@ def set_logging(lvl=logging.INFO):
 if __name__ == "__main__":
     # Create script arguments parser
     args_parse = ArgumentParser("Run photobooth.")
-    args_parse.add_argument("--output", "-o", metavar="folder", help="directory to store pictures", required= True)
+    args_parse.add_argument("--output", "-o", metavar="folder", help="directory to store pictures", required=True)
 
     params = args_parse.parse_args()
 
-    # Check output directory
+    # Check if output directory is writable.
     try:
         if os.path.isdir(params.output):
-            with open("{}/{}".format(params.output, "photobooth.txt", 'w')) as f:
-                f.write("")
+            open(os.path.join(params.output, "photobooth.txt"), 'a').close()
 
             # delete test file
             os.remove("{}/{}".format(params.output, "photobooth.txt"))
 
         else:
-            os.makedirs(params.output)
+            raise os.error("Folder does not exists.")
     except os.error as e:
         logging.error("Failed to write in {}: {}".format(params.output, e))
         exit(2)
-
-    camera = PiCamera()
-    camera.resolution = (800, 480)
-    camera.hflip = True
-    camera.start_preview(alpha=128)
-
-    # Create GUI with title.
-    app = App("Wedding Photobooth", 800, 480)
-    message = Text(app, "Smile !!!")
-    app.display()
