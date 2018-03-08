0bc36dfcdc42aa962fdb2f3472eaa509b7a4e9fb
Christophe <christophe.biguereau@gmail.com>
2018-03-08 20:54:14 +0100

    Use picamera preview to display current picture.

--- photobooth/libs/gui.py
+++ photobooth/libs/gui.py
@@ -127,7 +127,14 @@ class Gui:
 
             # Run capture loop.
             self.log4py.debug("run capture loop.")
-            for img in _cam.capture_continuous(stream, format='jpeg'):
+            while True:
+                _cam.start_preview()
+                if not self.stop_thread_event.is_set():
+                    break
+            _cam.stop_preview()
+
+            """
+            for img in _cam.capture_continuous(stream, format='jpeg', use_video_port=True):
 
                 # If close event is set, close
                 if not self.stop_thread_event.is_set():
@@ -138,6 +145,7 @@ class Gui:
                 tmpImage = PIL.Image.open(stream)
                 tmpImg = ImageTk.PhotoImage(tmpImage)
                 self.panel_video_stream.configure(image = tmpImg)
+            """
 
     def _start_cam_handler(self):
         """
