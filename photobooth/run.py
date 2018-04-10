60d273ac036f4af53d64e1e6bf50b2b828340e30
diablo02000 <christophe.biguereau@gmail.com>
2018-04-10 14:48:24 +0200

    Refactoring on logging section. Remove unused option verbose. Fix configuration file path.

--- photobooth/run.py
+++ photobooth/run.py
@@ -1,55 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import logging
+import logging.config
+import yaml
 import os
 import sys
 from argparse import ArgumentParser
 
-class PhotoboothException(Exception):
-    pass
+# Set modules and configuration path
+LIBS_PATH = "{}/libs/".format(os.path.dirname(os.path.realpath(__file__)))
+CONF_PATH = "{}/configurations/".format(os.path.dirname(os.path.realpath(__file__)))
+
 
 """
-    Try to import Photobooth apps libs.
+    Try to import logging configuration
+    and submodules.
 """
 try:
-    # Get current directory
-    libs_directory = "{}/libs/".format(os.path.dirname(os.path.realpath(__file__)))
+    # Init logging module
+    with open("{}logging.yaml".format(CONF_PATH), 'r') as f:
+        log_conf = yaml.load(f)
+        log_conf.setdefault('version', 1)
+        logging.config.dictConfig(log_conf)
 
-    sys.path.append(libs_directory)
+    logger = logging.getLogger(__name__)
+
+    # Load submodules
+    sys.path.append(LIBS_PATH)
 
     from gui import Gui
     from configuration import Configuration
+
 except Exception as e:
-    logging.error("Failed to import class from libs folder: {}".format(e))
-    exit(2)
+    raise Exception("Failed to load annexes.")
 
-# Define logger
-log4py = logging.getLogger('photobooth')
-
-def set_logging(lvl, log_path=None):
-    """
-    Set logging module
-    :param lvl: Log level.
-    :type lvl: Int
-    """
-    # define format
-    log_format = '%(asctime)-15s - %(levelname)s - %(message)s'
-    log_date_format = "%Y-%m-%d %H:%M:%S"
-    log_formatter = logging.Formatter(log_format, log_date_format)
-
-    # Create file handler if log path define
-    if log_path:
-        f_handler = logging.FileHandler(log_path)
-        f_handler.setFormatter(log_formatter)
-        log4py.addHandler(f_handler)
-    else:
-        # Create streamHandler
-        s_handler = logging.StreamHandler(sys.stdout)
-        s_handler.setFormatter(log_formatter)
-        log4py.addHandler(s_handler)
-
-    # Define log level
-    log4py.setLevel(lvl)
 
 def check_output_directory(directory):
     """
@@ -57,58 +40,51 @@ def check_output_directory(directory):
     :param directory: Path where to store pictures.
     """
     try:
-        testing_file = os.path.join(directory, "photobooth_apps.txt")
+        testing_file = os.path.join(directory, os.path.basename(__file__))
         open(testing_file, 'a').close()
 
         os.remove(testing_file)
     except NotADirectoryError:
-        raise PhotoboothException("{} not a valide directory.".format(directory))
+        logger.error("{} not a valide directory.".format(directory), exc_info=True)
+        exit(2)
     except Exception as e:
-        raise PhotoboothException("Unexpected error: {}".format(e))
+        logger.error("Unexpected error",  exc_info=True)
+        exit(2)
 
-def run(cfg, language, verbose):
+
+def run(lang):
     """
-        Run Photobooth application.
+    Run Photobooth application.
+    :param lang: Define with language to use
+    :type lang: str
     """
     # Init configuration
-    configuration = Configuration(cfg)
+    configuration = Configuration("{}config.yaml".format(CONF_PATH))
 
     # Check output directory
     check_output_directory(configuration.pictures_directory)
-
-    # Enable verbose log
-    if verbose:
-        log_lvl = logging.DEBUG
-    else:
-        log_lvl = logging.INFO
-
-    try:
-        set_logging(log_path=configuration.log, lvl=log_lvl)
-    except Exception:
-        set_logging(lvl=log_lvl)
 
     try:
         # Create Gui
         photobooth_app = Gui(configuration.resolution['width'],
                              configuration.resolution['height'],
-                             getattr(configuration, language),
+                             getattr(configuration, lang),
                              log4py)
 
         # Run Photobooth.
-        log4py.info("Photobooth apps running ...")
+        logger.info("Photobooth apps running ...")
         photobooth_app.run()
 
     except Exception as e:
-        log4py.error("Failed to run Photobooth app: {}".format(e))
+        logger.error("Failed to run Photobooth app: {}", exc_info=True)
         exit(1)
+
 
 if __name__ == "__main__":
     # Create script arguments parser
     args_parse = ArgumentParser("Run photobooth.")
-    args_parse.add_argument("--config", "-c", metavar="file", help="Configuration file", required=True)
     args_parse.add_argument("--language", "-l", metavar="language", default="fr", help="choose application language.")
-    args_parse.add_argument("--verbose", "-V", action="store_true")
 
     params = args_parse.parse_args()
 
-    run(cfg=params.config, language=params.language, verbose=params.verbose)
+    run(lang=params.language)
