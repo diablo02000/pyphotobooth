23d1c7b2452f361e10c76e06fc2347975f711da6
diablo02000 <christophe.biguereau@gmail.com>
2021-09-11 18:57:21 +0200

    fix: Update custom path in init conf.

--- pyphotobooth/__init__.py
+++ pyphotobooth/__init__.py
@@ -2,8 +2,8 @@
     Python photobooth applications for Raspberry PI.
 """
 
-from photobooth.libs.gui import Gui
-from photobooth.libs.configuration import Configuration
+from .libs.gui import Gui
+from .libs.configuration import Configuration
 
 __version__ = '0.1'
 
--- pyphotobooth/libs/__init__.py
+++ pyphotobooth/libs/__init__.py
@@ -2,5 +2,5 @@
 Load custom libs for Photobooth.
 """
 
-from gui import Gui
-from configuration import Configuration
+from .gui import Gui
+from .configuration import Configuration
